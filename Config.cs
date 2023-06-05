cc9af011f39f2e36a7eb938ad9ac030bd562f5a8
James <james@jcdc.dev>
2023-06-05 19:47:18 +0100

    seperate values per shelf storage type

---
+++ Config.cs
@@ -0,0 +1,13 @@
+namespace xdjoshuaaz {
+    public static class Config {
+        public static class ShelfLifeOfFoodStorage {
+			// Tier 1
+			public static readonly float StorageSilo = 1.5f;
+            public static readonly float Icebox = 1.5f;
+			//Tier 2
+			public static readonly float PoweredStorageSilo = 2.0f;
+        	public static readonly float Refrigerator = 2.0f;
+			public static readonly float IndustrialRefrigerator = 2.2f;
+        }
+    }
+}
